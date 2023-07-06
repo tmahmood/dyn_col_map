0b313fb2c77167e20e1ee0ab13128c2913356758
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2023-07-06 21:04:28 +0600

    Updated readme and some tests

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Column Mapper
 
+**this name may not be final**
+
 HashMap, BTreeMap, IndexMap needs a lot of memory in case of String based keys and large number of data.
 
 This is a simple library that tries to memory efficiently provide a `IndexMap` like functionality, that might have a large number of data with string keys, using vecs. There might be other better solutions in the wild. In my own testing, I was able to reduce memory usage more than half with a dataset of 947300 rows. As vec index are mapped with String based keys, we keep the best of both worlds. I have not benchmarked it, so can not say anything about performance.
+
 
 Simple macros are provided to easy assigning of data.
 
@@ -34,6 +37,125 @@ fn main() {
 }
 ```
 
+This also provides benefit when generating CSV files, with different dataset which may not have similar columns.
+So, if you have one dataset with columns `c1` and `c2` another with `c5` and `c6`, all you have to do is following
+
+```rust
+fn main() {
+    let mut cm = ColumnMapper::new();
+    // first dataset, but you can add all of the columns beforehand as usual
+    // cm.add_columns(vec!["c0", "c1", "c4", "c5"]);
+    
+    cm.add_columns(vec!["c0", "c1"]);
+    let mut p = Vec::new();
+    // insert data for first dataset
+    p.push(vec![]);
+    cl! {
+            ins cm, p[0],
+            kv "c0", "c0v",
+            kv "c1", "Something"
+        }
+    // now another dataset found
+    cm.add_columns(vec!["c4", "c5"]);
+    
+    // insert data for second dataset
+    p.push(vec![]);
+    cl! {
+            ins cm, p[1],
+            kv "c4", "v2",
+            kv "c5", "32"
+        }
+    
+    p.push(vec![]);
+    cl! {
+            ins cm, p[2],
+            kv "c1", "another set",
+            kv "c5", "mixed dataset"
+        }
+    assert_eq!(
+        p,
+        vec![
+            vec!["c0v", "Something"], // NOTE: this is not filled up. how to handle it is discussed next
+            vec!["", "", "v2", "32"],
+            vec!["", "another set", "", "mixed dataset"],
+        ]
+    );
+}
+```
+
+One issue is, as noted in the example above, any rows before a new column is added, will fail, as the column does not exist on them. Any rows added after will have them. To solve this issue, `fill_to_end` method should be used for each row as necessary. 
+
+Following example provided to clear the issue and solution.
+ 
+```rust
+    fn main() {
+    let mut cm = ColumnMapper::new();
+    cm.add_columns(vec!["c0", "c1", "c2", "c3"]);
+    let mut rows = Vec::new();
+
+    rows.push(vec![]);
+    cl! {
+            ins cm, rows[0],
+            kv "c0", "r1d0",
+            kv "c2", "r1d2"
+        }
+
+    // now a new column is added
+    cm.add_column("c4");
+
+    // this will cause a NoDataSet error, cause column c4 was created after setting this row, and it does not exists
+    let n = cm.get(&rows[0], "c4");
+    assert!(n.is_err());
+
+    // fill the row with
+    cm.fill_to_end(&mut rows[0]);
+    // now it will be okay
+    let n = cm.get(&rows[0], "c4");
+    assert!(n.is_ok());
+
+    // all the next rows will have all the columns
+    rows.push(vec![]);
+    cl! {
+            ins cm, rows[1],
+            kv "c0", "r2d0",
+            kv "c2", "r2d2"
+        }
+
+    // this will work without filling up
+    let n = cm.get(&rows[1], "c4");
+    assert!(n.is_ok());
+
+    println!("{:?}", rows);
+}
+```
+
+Here's how to save data to a CSV file using `csv` crate
+
+```rust
+
+pub fn write_to_csv(
+    file_name: PathBuf,
+    rows: &Vec<Vec<String>>,
+    col_mapper: &ColumnMapper,
+) {
+    let mut writer = WriterBuilder::new()
+        .has_headers(false)
+        .from_path(file_name)
+        .unwrap();
+    let all_columns = col_mapper.get_columns();
+    writer.write_record(&all_columns).unwrap();
+    for row in rows.iter() {
+        if row.len() != all_columns.len() {
+            let mut new_row = row.clone();
+            col_mapper.fill_to_end(&mut new_row);
+            writer.write_record(new_row).unwrap()
+        } else {
+            writer.write_record(row).unwrap()
+        }
+    }
+    writer.flush().unwrap();
+}
+```
 
 ## What this crate tries to solve?
 
--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,17 @@ mod tests {
     fn test_col_mapper_macro() {
         let mut cm = ColumnMapper::new();
         cm.add_columns(vec!["c0", "c1", "c2", "c3"]);
-        let mut p = vec![];
-        cl! { ins cm, p, "c0", "c0v" }
+        let mut row = vec![];
+        cl! { ins cm, row, "c0", "c0v" }
         cl! {
-            ins cm, p,
+            ins cm, row,
             kv "c1", "Something",
             kv "c2", "v2",
             kv "c3", "32"
         }
+        // get all the columns, sequence is maintained
         assert_eq!(cm.get_columns(), vec!["c0", "c1", "c2", "c3"]);
-        assert_eq!(p, vec!["c0v", "Something", "v2", "32"])
+        assert_eq!(row, vec!["c0v", "Something", "v2", "32"])
     }
 
     #[test]
@@ -36,50 +37,52 @@ mod tests {
         ];
         let mut column_mapper = ColumnMapper::new();
         column_mapper.add_columns(vec!["c0", "c1", "c2", "c3"]);
-        let mut p = vec![];
-        cl! { ins column_mapper, p, "c0", ar[0].clone() }
+        let mut row = vec![];
+        cl! { ins column_mapper, row, "c0", ar[0].clone() }
         cl! {
-            ins column_mapper, p,
+            ins column_mapper, row,
             kv "c1", ar[1].clone(),
             kv "c2", ar[2].clone(),
             kv "c3", ar[3].clone()
         }
         assert_eq!(column_mapper.get_columns(), vec!["c0", "c1", "c2", "c3"]);
-        assert_eq!(p, ar)
+        assert_eq!(row, ar)
     }
 
     #[test]
     fn test_insert_randomly() {
         let mut cm = ColumnMapper::new();
         cm.add_columns(vec!["c0", "c1", "c2", "c3"]);
-        let mut p = Vec::new();
+        let mut row = Vec::new();
 
-        cm.insert(&mut p, "c1", "Something").unwrap();
-        cm.insert(&mut p, "c3", "Another thing").unwrap();
-        cm.insert(&mut p, "c2", "First thing").unwrap();
-
-        assert_eq!(cm.get(&p, "c1").unwrap(), "Something");
-
-        assert!(cm.get(&p, "c10").is_err());
-
-        assert_eq!(p, vec!["", "Something", "First thing", "Another thing"]);
+        cl! {
+            ins cm, row,
+            kv "c1", "Something",
+            kv "c3", "Another thing",
+            kv "c2", "First thing"
+        }
+        assert_eq!(cm.get(&row, "c1").unwrap(), "Something");
+        assert!(cm.get(&row, "c10").is_err());
+        assert_eq!(row, vec!["", "Something", "First thing", "Another thing"]);
     }
 
     #[test]
     fn test_extending_with_new_column() {
         let mut cm = ColumnMapper::new();
         cm.add_columns(vec!["c0", "c1", "c2", "c3"]);
-        let mut p = Vec::new();
-
-        cm.insert(&mut p, "c1", "Something").unwrap();
-        cm.insert(&mut p, "c3", "Another thing").unwrap();
-        cm.insert(&mut p, "c2", "First thing").unwrap();
-
+        let mut row = Vec::new();
+        cl! {
+            ins cm, row,
+            kv "c1", "Something",
+            kv "c3", "Another thing",
+            kv "c2", "First thing"
+        }
         cm.add_column("c5");
-        cm.insert(&mut p, "c0", "First First thing").unwrap();
+        cm.insert(&mut row, "c0", "First First thing").unwrap();
 
+        // no matter how the data is inserted, the sequence of column is maintained
         assert_eq!(
-            p,
+            row,
             vec![
                 "First First thing",
                 "Something",
@@ -94,36 +97,113 @@ mod tests {
     fn test_multiple_row_with_empty_column() {
         let mut cm = ColumnMapper::new();
         cm.add_columns(vec!["c0", "c1", "c2", "c3"]);
-        let mut p = Vec::new();
-        p.insert(0, vec![]);
+        let mut rows = Vec::new();
+        rows.insert(0, vec![]);
         cl! {
-            ins cm, p[0],
+            ins cm, rows[0],
             kv "c0", "c0v",
             kv "c1", "Something",
             kv "c2", "v2",
             kv "c3", "32"
         }
-        p.insert(1, vec![]);
+        rows.insert(1, vec![]);
         cl! {
-            ins cm, p[1],
+            ins cm, rows[1],
             kv "c0", "c0v",
             kv "c2", "v2",
             kv "c3", "32"
         }
-        p.insert(2, vec![]);
+        rows.insert(2, vec![]);
         cl! {
-            ins cm, p[2],
+            ins cm, rows[2],
             kv "c0", "c0v",
             kv "c1", "Something",
             kv "c2", "v2"
         }
         assert_eq!(
-            p,
+            rows,
             vec![
                 vec!["c0v", "Something", "v2", "32"],
                 vec!["c0v", "", "v2", "32"],
                 vec!["c0v", "Something", "v2", ""],
             ]
         );
+    }
+
+    #[test]
+    fn test_multi_datasets_csv() {
+        let mut cm = ColumnMapper::new();
+        cm.add_columns(vec!["c0", "c1"]);
+        let mut rows = Vec::new();
+        // insert data for first dataset
+        rows.push(vec![]);
+        cl! {
+            ins cm, rows[0],
+            kv "c0", "c0v",
+            kv "c1", "Something"
+        }
+        cm.add_columns(vec!["c4", "c5"]);
+        // insert data for second dataset
+        rows.push(vec![]);
+        cl! {
+            ins cm, rows[1],
+            kv "c4", "v2",
+            kv "c5", "32"
+        }
+        // mixture of dataset is possible
+        rows.push(vec![]);
+        cl! {
+            ins cm, rows[2],
+            kv "c1", "another set",
+            kv "c5", "mixed dataset"
+        }
+        assert_eq!(
+            rows,
+            vec![
+                vec!["c0v", "Something"],
+                vec!["", "", "v2", "32"],
+                vec!["", "another set", "", "mixed dataset"],
+            ]
+        );
+    }
+
+    #[test]
+    fn test_handling_unset_columns() {
+        let mut cm = ColumnMapper::new();
+        cm.add_columns(vec!["c0", "c1", "c2", "c3"]);
+        let mut rows = Vec::new();
+
+        rows.push(vec![]);
+        cl! {
+            ins cm, rows[0],
+            kv "c0", "r1d0",
+            kv "c2", "r1d2"
+        }
+
+        cm.add_column("c4");
+
+        // this will cause a NoDataSet error, cause column c4 was created after setting *this* row
+        let n = cm.get(&rows[0], "c4");
+        assert!(n.is_err());
+
+        // fill the row with
+        cm.fill_to_end(&mut rows[0]);
+        // now it will be okay
+        let n = cm.get(&rows[0], "c4");
+        assert!(n.is_ok());
+
+        // all the next rows will have all the columns
+        rows.push(vec![]);
+        cl! {
+            ins cm, rows[1],
+            kv "c0", "r2d0",
+            kv "c2", "r2d2"
+        }
+
+        // this will work without filling up
+        let n = cm.get(&rows[1], "c4");
+        assert!(n.is_ok());
+
+        println!("{:?}", rows);
     }
 }
